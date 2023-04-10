7c278ac03bd61177408220b075da7d26eebc0290
Billy Hoang <billyhoang100@gmail.com>
2023-04-10 10:42:02 -0500

    image and data display set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [endPoint, setEndPoints] = useState('');
   const [container, setContainer] = useState([]);
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'bc14076dbfmsh75a0c9ef14c5cd5p1f98e0jsn58abf0c19ef1',
-      'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
-    },
+  const fetchMe = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'bc14076dbfmsh75a0c9ef14c5cd5p1f98e0jsn58abf0c19ef1',
+        'X-RapidAPI-Host': 'imdb8.p.rapidapi.com',
+      },
+    };
+
+    fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, options)
+      .then((response) => {
+        console.log('work');
+        return response.json();
+      })
+      .then((data) => {
+        setContainer(data.d);
+      })
+      .catch((err) => console.error(err));
   };
 
-  fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .then((data) => {
-      setContainer(data);
-    })
-    .catch((err) => console.error(err));
+  useEffect(() => {
+    fetchMe();
+  }, [endPoint]);
 
   const onChangeHandler = (e) => {
     setEndPoints(e.target.value);
@@ -31,9 +39,17 @@ function App() {
   return (
     <div>
       <form onSubmit={submitHandler}>
-        <input type="text" value={endPoint} onChange={setEndPoints} />
+        <input type="text" value={endPoint} onChange={onChangeHandler} />
         <button type="submit">submit</button>
       </form>
+
+      {container.map((item) => (
+        <div>
+          <img src={item.i.imageUrl} alt="" />
+          <p>{item.l}</p>
+        </div>
+      ))}
+
     </div>
   );
 }
